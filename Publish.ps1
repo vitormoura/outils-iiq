7b1f7831fceea1ed075d2b3c79276fff8fa3b86e
Vitor Moura <663103+vitormoura@users.noreply.github.com>
2024-08-08 14:23:03 -0400

    Update Publish.ps1

--- Publish.ps1
+++ Publish.ps1
@@ -3,4 +3,8 @@ $DebugPreference = 'Continue';
 
 Import-Module ./scripts/IdentityIQ-FunctionsForDev.psm1
 
+if(-not (Test-Path ./temp)) {
+	mkdir ./temp
+}
+
 Export-IIQRuleFiles -PathJavaFiles "./src/rules/**/*.java" -OutputDir './temp'
